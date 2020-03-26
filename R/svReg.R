32068662561d19e1a2437ce4a2b2704e22a5fd16
rakheon <rakheon.kim@gmail.com>
2020-03-25 22:01:55 -0500

    revised cv.svReg documentation

--- R/svReg.R
+++ R/svReg.R
@@ -393,7 +393,7 @@ svReg1 <- function(X, Z = NULL, Y, df_X, df_Z, lambda = 0.5, alpha = 0.5, tt = 0
 }
 
 
-#' Fit the continuous-categorical pliable lasso (c2plasso) in the linear regression setting
+#' Fit the structural varying-coefficient regression (svReg) in the linear regression setting
 #'
 #' @param X N by p matrix of main predictors
 #' @param Z N by K matrix of modifying variables. Modifying variables can take the form of continuous variables or categorical variables or both. Categorical variable should be coded by dummy variables (0-1).
@@ -480,12 +480,13 @@ svReg <- function(X, Z = NULL, Y, df_X, df_Z, lambda_seq = NULL, alpha = 0.5, tt
 }
 
 
-#' Perform k-fold cross validation for the continuous-categorical pliable lasso (c2plasso) model over a sequence of regularization parameter
+#' Perform k-fold cross validation for the structural varying-coefficient regression (svReg) model over a sequence of regularization parameter
 #'
 #' @param X N by p matrix of main predictors
 #' @param Z N by K matrix of modifying variables. Modifying variables can take the form of continuous variables or categorical variables or both. Categorical variable should be coded by dummy variables (0-1).
 #' @param Y vector of response variable
-#' @param df_Z vector of degrees of freedom for each group of modifying variables. Continuous or binary variables has one degree of freedom. Categorical variables with C categories has (C-1) degrees of freedom. For example, if there are one continuous modifying variable, one binary modifying variable and one categorical modifying variable with 4 factor levels which is expressed with 3 binary dummy variables, then df_Z = c(1,1,3).
+#' @param df_X vector of degrees of freedom for each group of main predictors.
+#' @param df_Z vector of degrees of freedom for each group of modifying variables.
 #' @param kfold the number of folds (=k) for the k-fold cross-validation. Default value is 10.
 #' @param lambda_seq sequence of the tuning parameter, lambda. Can take the form of a sequence or a scalar.
 #' @param alpha weight parameter between group penalty and individual penalty. Default value is 0.5.
@@ -520,10 +521,13 @@ svReg <- function(X, Z = NULL, Y, df_X, df_Z, lambda_seq = NULL, alpha = 0.5, tt
 #' z=cbind(z1, z2)
 #' z=as.matrix(z)
 #' y=2*x[,1] - (2+2*z[,1])*x[,2] + (2+3*z[,4]+2*z[,5]-2*z[,6])*x[,3] + rnorm(100, 0, 1)
-#' cv.c2plasso(X = x, Z = z, Y = y, df_Z = c(1,1,1,3,3), lambda_seq = c(1, 0.5), alpha = 0.5)
-#' cv.c2plasso(X = x, Z = z, Y = y, df_Z = c(1,1,1,3,3), lambda_seq = c(1, 0.5), alpha = 0.5, cvseed = 1234)
-#' cv.c2plasso(X = x, Z = z, Y = y, df_Z = c(1,1,1,3,3), lambda_seq = 0.5, alpha = 0.5)
-#' cv.c2plasso(X = x, Z = z, Y = y, df_Z = c(1,1,1,3,3), lambda_seq = 0.5, alpha = 0.5, zlinear = FALSE)
+#' cv.svReg_res1 = cv.svReg(X = x, Z = z, Y = y, df_X = rep(1,5), df_Z = c(1,1,1,3,3), lambda_seq = c(1, 0.5), alpha = 0.5)
+#' cv.svReg_res2 = cv.svReg(X = x, Z = z, Y = y, df_X = rep(1,5), df_Z = c(1,1,1,3,3), lambda_seq = c(1, 0.5), alpha = 0.5, cvseed = 1234)
+#' cv.svReg_res3 = cv.svReg(X = x, Z = z, Y = y, df_X = rep(1,5), df_Z = c(1,1,1,3,3), lambda_seq = 0.5, alpha = 0.5)
+#' cv.svReg_res4 = cv.svReg(X = x, Z = z, Y = y, df_X = rep(1,5), df_Z = c(1,1,1,3,3), lambda_seq = 0.5, alpha = 0.5, zlinear = FALSE)
+#' x[,3] = 2/3*x[,1] + 2/3*x[,2] + 1/3*rnorm(100, 0, 1)
+#' y = x[,1] + x[,2] + (2+3*z[,4]+2*z[,5]-2*z[,6])*x[,4] + rnorm(100, 0, 1)
+#' cv.svReg_res5 = cv.svReg(X = x, Z = z, Y = y, df_X = c(3,1,1), df_Z = c(1,1,1,3,3), lambda_seq = c(1, 0.5), alpha = 0.5)
 cv.svReg <- function(X, Z = NULL, Y, df_X, df_Z, kfold = 10, lambda_seq = NULL, alpha = 0.5, tt = 0.1, zlinear = TRUE, tol = 1e-7, iter = 500, cvseed = NULL){
 
     # p: number of main predictors; K: number of modifying variables, N: sample size
@@ -590,315 +594,4 @@ cv.svReg <- function(X, Z = NULL, Y, df_X, df_Z, kfold = 10, lambda_seq = NULL, 
     theta0_raw_vec <- c2plassofit$theta0_raw_vec
 
     return(list(lambda_seq = lambda_seq, beta_mat = beta_mat, beta0_vec = beta0_vec, theta_mat = theta_mat, theta0_vec = theta0_vec, beta_raw_mat = beta_raw_mat, beta0_raw_vec = beta0_raw_vec, theta_raw_mat = theta_raw_mat, theta0_raw_vec = theta0_raw_vec, lambda_min = lambda_min, lambda_1se = lambda_1se, cvm = cvm, cvse = cvse, cvfold = cvfold, sqerror = sqerror))
-}
-
-#dyn.load("/Users/junkyungkim/Desktop/R_packages/SGL/src/Coxfit.so")
-#dyn.load("/Users/junkyungkim/Desktop/R_packages/SGL/src/registerDynamicSymbol.so")
-if (FALSE){
-oneDim <-
-    function(data, index, thresh = 0.0001, nlam = 20, lambdas = NULL, beta.naught = rep(0,ncol(data$x)), inner.iter = 100, outer.iter = 100, outer.thresh = 0.0001, gamma = 0.8, step = 1, reset = 10, alpha = 0.95, min.frac = 0.05, verbose = FALSE){
-
-        if(is.null(lambdas)){
-            lambdas <- betterPathCalc(data = data, index = index, alpha=alpha, min.frac = min.frac, nlam = nlam, type = "linear")
-        }
-
-        X <- data$x
-        y <- data$y
-        n <- nrow(X)
-        p <- ncol(X)
-
-        ## Setting up group lasso stuff ##
-
-        ord <- order(index)
-        index <- index[ord]
-        X <- X[,ord]
-        unOrd <- match(1:length(ord),ord)
-
-        ## Coming up with other C++ info ##
-
-        groups <- unique(index)
-        num.groups <- length(groups)
-        range.group.ind <- rep(0,(num.groups+1))
-        for(i in 1:num.groups){
-            range.group.ind[i] <- min(which(index == groups[i])) - 1
-        }
-        range.group.ind[num.groups+1] <- ncol(X)
-
-        group.length <- diff(range.group.ind)
-
-        ## DONE SETTING UP C STUFF ##
-
-        #alpha <- sqrt(2*log(p))/(1+sqrt(2*log(num.groups)/min(group.length)) + sqrt(2*log(p)))
-
-        nlam = length(lambdas)
-        beta.old <- rep(0,ncol(X))
-        beta.is.zero <- rep(1,num.groups)
-        beta <- array(0, c(ncol(X),nlam))
-
-        eta <- rep(0,n)
-
-        for(k in 1:nlam){
-            beta.is.zero <- rep(1, num.groups)
-            beta.old <- rep(0, ncol(X))
-            eta <- rep(0,n)
-
-            junk <- .C("linNest", X = as.double(as.vector(X)), y = as.double(y), index = as.integer(index), nrow = as.integer(nrow(X)), ncol = as.integer(ncol(X)), numGroup = as.integer(num.groups), rangeGroupInd = as.integer(range.group.ind), groupLen = as.integer(group.length), lambda1 = as.double(lambdas[k]*alpha), lambda2 = as.double(lambdas[k]*(1-alpha)), beta = as.double(beta.old), innerIter = as.integer(inner.iter), outerIter = as.integer(outer.iter), thresh = as.double(thresh), outerThresh = as.double(outer.thresh), eta = as.double(eta), gamma = as.double(gamma), betaIsZero = as.integer(beta.is.zero), step = as.double(step), reset = as.integer(reset))
-
-            beta.new <- junk$beta
-            beta[,k] <- beta.new
-            beta.is.zero <- junk$betaIsZero
-            eta <- junk$eta
-            beta.old <- beta.new
-            if(verbose == TRUE){
-                write(paste("***Lambda", k, "***"),"")
-            }
-        }
-        return(list(beta = beta[unOrd,], lambdas = lambdas))
-    }
-
-
-
-betterPathCalc <- function(data, index, alpha = 0.95, min.frac = 0.05, nlam = 20, type = "linear"){
-
-    reset <- 10
-    step <- 1
-    gamma <- 0.8
-
-    inner.iter <- 1000
-    outer.iter <- 1000
-    thresh = 10^(-3)
-    outer.thresh = thresh
-
-    n <- nrow(data$x)
-
-    if(type == "linear"){
-
-        X <- data$x
-        resp <- data$y
-        n <- nrow(X)
-        p <- ncol(X)
-
-        ## Setting up group lasso stuff ##
-
-        ord <- order(index)
-        index <- index[ord]
-        X <- X[,ord]
-        unOrd <- match(1:length(ord),ord)
-
-        ## Coming up with other C++ info ##
-
-        groups <- unique(index)
-        num.groups <- length(groups)
-        range.group.ind <- rep(0,(num.groups+1))
-        for(i in 1:num.groups){
-            range.group.ind[i] <- min(which(index == groups[i])) - 1
-        }
-        range.group.ind[num.groups + 1] <- ncol(X)
-
-        group.length <- diff(range.group.ind)
-
-    }
-
-    if(type == "logit"){
-
-        X <- data$x
-        y <- data$y
-        n <- nrow(X)
-        p <- ncol(X)
-
-        ## Setting up group lasso stuff ##
-
-        ord <- order(index)
-        index <- index[ord]
-        X <- X[,ord]
-        unOrd <- match(1:length(ord),ord)
-
-        ## Coming up with other C++ info ##
-
-        groups <- unique(index)
-        num.groups <- length(groups)
-        range.group.ind <- rep(0,(num.groups+1))
-        for(i in 1:num.groups){
-            range.group.ind[i] <- min(which(index == groups[i])) - 1
-        }
-        range.group.ind[num.groups + 1] <- ncol(X)
-
-        group.length <- diff(range.group.ind)
-        beta.naught <- rep(0,ncol(X))
-        beta <- beta.naught
-
-        beta.is.zero <- rep(1, num.groups)
-        beta.old <- rep(0, ncol(X))
-        betas <- matrix(0, nrow = ncol(X), ncol = nlam)
-
-        eta <- rep(0,n)
-
-        intercepts <- mean(y)
-
-        eta = eta + intercepts
-
-        m.y <- mean(y)
-
-        resp <- m.y*m.y*(1-m.y) - (y-m.y)
-
-    }
-
-    if(type == "cox"){
-
-        covariates <- data$x
-        n <- nrow(covariates)
-        p <- ncol(covariates)
-        time <- data$time
-        status <- data$status
-
-        ## Ordering Response and Removing any Censored obs before first death ##
-        death.order <- order(time)
-        ordered.time <- sort(time)
-
-        X <- covariates[death.order,]
-        ordered.status <- status[death.order]
-
-        first.blood <- min(which(ordered.status == 1))
-
-        X <- X[first.blood:n,]
-        ordered.status <- ordered.status[first.blood:n]
-        ordered.time <- ordered.time[first.blood:n]
-        death.order <- death.order[first.blood:n]
-        n <- n-first.blood+1
-
-        death.times <- unique(ordered.time[which(ordered.status == 1)])  ## Increasing list of times when someone died (censored ends not included) ##
-
-        ## Calculating Risk Sets ##
-
-        risk.set <- rep(0,n)
-        for(i in 1:n){
-            risk.set[i] <- max(which(death.times <= ordered.time[i]))
-        }
-
-        ## Calculating risk set beginning/ending indices ##
-
-        risk.set.ind <- rep(0,(length(death.times)+1))
-        for(i in 1:length(death.times)){
-            risk.set.ind[i] <- min(which(ordered.time >= death.times[i]))
-        }
-        risk.set.ind[length(risk.set.ind)] <- length(ordered.time) + 1
-
-        ## Calculating number of deaths at each death time ##
-        num.deaths <- rep(0,length(death.times))
-        for(i in 1:length(ordered.time)){
-            if(ordered.status[i] == 1){
-                num.deaths[which(death.times == ordered.time[i])] <-  num.deaths[which(death.times == ordered.time[i])] + 1
-            }
-        }
-
-        ## Finding death indices and number of deaths ##
-        death.index <- which(ordered.status == 1)
-        total.deaths <- length(death.index)
-
-        ## Setting up group lasso stuff ##
-
-        ord <- order(index)
-        index <- index[ord]
-        X <- X[,ord]
-        unOrd <- match(1:length(ord),ord)
-
-        ## Coming up with other C++ info ##
-
-        groups <- unique(index)
-        num.groups <- length(groups)
-        range.group.ind <- rep(0,(num.groups+1))
-        for(i in 1:num.groups){
-            range.group.ind[i] <- min(which(index == groups[i])) - 1
-        }
-        range.group.ind[num.groups + 1] <- ncol(X)
-
-        group.length <- diff(range.group.ind)
-        beta.naught <- rep(0,ncol(X))
-        beta <- beta.naught
-
-        beta.is.zero <- rep(1, num.groups)
-        beta.old <- rep(0, ncol(X))
-        beta <- array(0, c(ncol(X),nlam,nlam))
-
-        beta.is.zero <- rep(1, num.groups)
-
-        eta <- rep(0,n)
-
-        ## DONE SETTING UP COX MODEL STUFF
-
-
-        junk1 <- .C("Cox", riskSetInd = as.integer(risk.set.ind), riskSet = as.integer(risk.set), numDeath = as.integer(num.deaths), status = as.integer(ordered.status), ndeath = as.integer(length(death.times)), nrow = as.integer(n), ncol = as.integer(p), beta = as.double(rep(0,p)), eta = as.double(rep(0,n)), y = as.double(rep(0,n)), weights = as.double(rep(0,n)))
-
-        resp <- junk1$y * junk1$weights
-    }
-    lambda.max <- rep(0,num.groups)
-
-    if((alpha != 0)*(alpha != 1)){
-
-        for(i in 1:num.groups){
-            ind <- groups[i]
-            X.fit <- X[,which(index == ind)]
-            cors <- t(X.fit) %*% resp
-            ord.cors <- sort(abs(cors), decreasing = TRUE)
-
-            if(length(ord.cors) > 1){
-                norms <- rep(0,length(cors)-1)
-
-                lam <- ord.cors/alpha
-
-
-
-                for(j in 1:(length(ord.cors)-1)){
-                    norms[j] <- sqrt(sum((ord.cors[1:j]-ord.cors[j+1])^2))
-                }
-                if(norms[1] >= lam[2] * (1-alpha)*sqrt(group.length[i])){
-                    our.cors <- ord.cors[1]
-                    our.range <- c(ord.cors[2], ord.cors[1])/alpha
-                }else{
-                    if(norms[length(ord.cors)-1] <= lam[length(ord.cors)] * (1-alpha)*sqrt(group.length[i])){
-                        our.cors <- ord.cors
-                        our.range <- c(0, ord.cors[length(ord.cors)])/alpha
-                    } else{
-                        my.ind <- max(which(norms[-length(norms)] <= lam[2:(length(norms))] * (1-alpha) * sqrt(group.length[i]))) + 1
-                        our.cors <- ord.cors[1:my.ind]
-                        our.range <- c(ord.cors[my.ind+1], ord.cors[my.ind])/alpha
-                    }
-                }
-                nn <- length(our.cors)
-                if(alpha == 0.5){
-                    alpha = 0.500001
-                }
-
-                A.term <- nn*alpha^2 - (1 - alpha)^2*group.length[i]
-                B.term <- - 2 * alpha * sum(our.cors)
-                C.term <- sum(our.cors^2)
-
-                lams <- c((-B.term + sqrt(B.term^2 - 4 * A.term * C.term))/(2*A.term), (-B.term - sqrt(B.term^2 - 4 * A.term * C.term))/(2*A.term))
-
-
-                lambda.max[i] <- min(subset(lams, lams >= our.range[1] & lams <= our.range[2]))
-            }
-            if(length(ord.cors) == 1){
-                lambda.max[i] <- ord.cors
-            }
-        }
-    }
-    if(alpha == 1){
-        lambda.max <- abs(t(X) %*% resp)
-    }
-    if(alpha == 0){
-        for(i in 1:num.groups){
-            ind <- groups[i]
-            X.fit <- X[,which(index == ind)]
-            cors <- t(X.fit) %*% resp
-            lambda.max[i] <- sqrt(sum(cors^2)) / sqrt(group.length[i])
-        }
-    }
-    max.lam <- max(lambda.max)
-    min.lam <- min.frac*max.lam
-    lambdas <- exp(seq(log(max.lam),log(min.lam), (log(min.lam) - log(max.lam))/(nlam-1)))
-    return(lambdas/nrow(X))
-}
-
-
 }
